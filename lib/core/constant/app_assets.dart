e7689faff41b4bf2572b0709e4274642f66e5a80
Mohammed M El-tohamy <mohtohamy@inbox.ru>
2024-03-08 20:08:14 +0200

    add svgs from figma to assets

---
+++ lib/core/constant/app_assets.dart
@@ -0,0 +1,18 @@
+class AppAssets {
+  AppAssets._(); //متاخدش اي اوبجيكت من الكلاس دا
+
+ static const String logoHt = "assets/svgs/ht_logo.svg";
+ static const String logoVr = "assets/svgs/vr_logo.svg";
+ static const String forgetMyPassword = "assets/svgs/my_password_cuate.svg";
+
+  /*
+  *
+  * assets/svgs/ht_logo.svg
+  *
+  * AppAssets.logo
+  *
+  *
+  * */
+
+
+}
